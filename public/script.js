4488245ecebca5dde27d17eb5882204a0fdc86c8
vagdonic <manavvg@gmail.com>
2021-11-02 12:24:12 +0530

    cleaning and building

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ const BOT_MSGS = [
   "Hi, how are you?",
   "Ohh... I can't understand what you trying to say. Sorry!",
   "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
+  "Sorry if my answers are not relevant. :)",
   "I feel sleepy! :("
 ];
 
